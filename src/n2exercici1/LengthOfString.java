e05c6169ecc02fe06d1044212fd00c1ea66c6440
anderson92zolis <azolis92@hotmail.es>
2023-01-22 10:43:29 +0100

    N2E1 hecho

---
+++ src/n2exercici1/LengthOfString.java
@@ -0,0 +1,8 @@
+package n2exercici1;
+
+public class LengthOfString {
+
+	public static int LengthOfString(String word) {
+		return word.length();
+		}
+}
---
+++ src/n2exercici1/LengthOfStringTest.java
@@ -0,0 +1,23 @@
+package n2exercici1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.hamcrest.Matcher;
+import org.hamcrest.FeatureMatcher;
+import org.junit.jupiter.api.Test;
+
+class LengthOfStringTest {
+	
+	public static Matcher<String> length(Matcher<? super Integer> matcher){
+		return new FeatureMatcher<String, Integer>(matcher,"Longitud de String correcto","longitud de String incorrecta"){
+			@Override
+			protected Integer featureValueOf(String palabra) {
+				return palabra.length();
+			}
+		};
+	}
+	@Test
+	void testLengthOfString() {
+		assertEquals(6,LengthOfString.LengthOfString("MORDOR"));
+	}
+
+}
